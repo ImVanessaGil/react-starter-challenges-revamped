75f0406bc10727698e7aab23b8de076961dd3119
Vanessa <karenvanesssa.gil@gmail.com>
2024-11-19 16:43:57 -0600

    Add main structure for videos

--- react-starter-challenges-revamped/src/pages/Video.tsx
+++ react-starter-challenges-revamped/src/pages/Video.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import { Card } from "@mui/material";
+import Cards from "../components/Cards";
+import TextField_search from "../components/TextField-search";
 
 const Video = () => {
-  return <div>Video</div>;
+  return (
+    <>
+      <Card
+        sx={{
+          height: "auto",
+          backgroundColor: "white",
+        }}
+      >
+        <div
+          style={{
+            color: "black",
+            marginTop: "30px",
+            marginBottom: "20px",
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="h3">React Videos</Typography>
+          <Typography variant="h6">A brief history of React</Typography>
+        </div>
+        <TextField_search />
+        <Cards />
+        <Cards />
+
+        <Cards />
+        <Cards />
+      </Card>
+    </>
+  );
 };
 
 export default Video;
