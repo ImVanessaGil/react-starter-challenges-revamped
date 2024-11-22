4b408953a9187a1cc58409962c6b56b2bb9dec0a
Vanessa <karenvanesssa.gil@gmail.com>
2024-11-22 15:07:40 -0600

    Change Router from App to main

--- react-starter-challenges-revamped/src/App.tsx
+++ react-starter-challenges-revamped/src/App.tsx
@@ -8,7 +8,16 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-   
+    <div>
+      <NavLayout />
+      <Routes>
+        <Route path="/tic_tac_toe" element={<Tic_tac_toe />} />
+        <Route path="/video" element={<Video />} />
+        <Route path="/github_clone" element={<Github_clone />} />
+        <Route path="*" element={<Default />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
--- react-starter-challenges-revamped/src/main.tsx
+++ react-starter-challenges-revamped/src/main.tsx
@@ -9,20 +9,12 @@ import Video from "./pages/Video";
 import Github_clone from "./pages/Github_clone";
 import Default from "./pages/Default";
 import Home from "./pages/Home";
+import AppRouter from "./router/AppRouter";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <div>
-        <NavLayout />
-        <Routes>
-          <Route path="/tic_tac_toe" element={<Tic_tac_toe />} />
-          <Route path="/video" element={<Video />} />
-          <Route path="/github_clone" element={<Github_clone />} />
-          <Route path="*" element={<Default />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <AppRouter />
     </BrowserRouter>
   </React.StrictMode>
 );
